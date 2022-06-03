6fe3314f792dc1f0c2ab5291c8f6f57f2acbf1ab
AnaMickaellyAguiar <ana.aguiar@seno-ti.com.br>
2022-06-03 17:57:50 -0300

    feat: add storage API

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,9 +1,9 @@
 // import { isPlatform } from '@ionic/react';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Directory, Filesystem } from '@capacitor/filesystem';
 import { useState } from 'react';
-// import {Filesystem, Directory} from '@capacitor/filesystem';
-// import { Storage } from '@capacitor/storage';
-// import {Capacitor} from '@capacitor/core'
+//  import { Storage } from '@capacitor/storage';
+//  import {Capacitor} from '@capacitor/core'
 
 export interface UserPhoto {
   filepath: string;
@@ -12,6 +12,41 @@ export interface UserPhoto {
 
 export function usePhotoGallery() {
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
+  const PHOTO_STORAGE = 'photos';
+
+  const savePicture = async (
+    photo: any,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
+
+  async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject('method did not return a string');
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
 
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -21,15 +56,8 @@ export function usePhotoGallery() {
     });
 
     const fileName = new Date().getTime() + '.jpg';
-
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-      },
-      ...photos,
-    ];
-
+    const savedFileImage = await savePicture(photo, fileName);
+    const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
   };
 
